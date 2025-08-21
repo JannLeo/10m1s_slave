21d7478b1fe5b5075c30f765cd4331dd0f3ee48c
JunnanLiu <1144097453@qq.com>
2025-08-21 19:15:18 +0800

    add DEVICE_INDEX to make the compile for 10 slave more convenient

--- tl_ble_sdk/vendor/acl_connection_demo/app.c
+++ tl_ble_sdk/vendor/acl_connection_demo/app.c
@@ -52,7 +52,7 @@ const u8 tbl_advData[] = {
     'o',
     'n',
     'n',
-    '4',
+    '0'+ DEVICE_INDEX,
     2,
     DT_FLAGS,
     0x05, // BLE limited discoverable mode and BR/EDR not supported
@@ -84,7 +84,7 @@ const u8 tbl_scanRsp[] = {
     'o',
     'n',
     'n',
-    '4',
+    '0'+ DEVICE_INDEX,
 };
 
 
--- tl_ble_sdk/vendor/acl_connection_demo/app_config.h
+++ tl_ble_sdk/vendor/acl_connection_demo/app_config.h
@@ -25,7 +25,7 @@
 
 #include "config.h"
 
-
+#define DEVICE_INDEX   9
 #define ACL_CENTRAL_MAX_NUM 0 // ACL central maximum number
 #define ACL_PERIPHR_MAX_NUM 1 // ACL peripheral maximum number
 
